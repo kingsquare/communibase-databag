adce0afe083f478396a07f2a10afac200c9428bb
Robin Speekenbrink <robin@kingsquare.nl>
2020-10-23 23:57:51 +0200

    fix: aligned test suite to snakecaseing
    Some of the test were snake_cased while others were camelCased. This is odd. I aligned them and also moved the structure a bit to be consistent across tests. (teardowns et al)
    
    Signed-off-by: Robin Speekenbrink <robin@kingsquare.nl>

--- tests/GetFromDataBagTest.php
+++ tests/GetFromDataBagTest.php
@@ -16,6 +16,52 @@ final class GetFromDataBagTest extends TestCase
 {
     private $dataBag;
 
+    public function invalidPathProvider(): array
+    {
+        return [
+            'empty' => [''],
+            'non-existant' => ['invalidPath'],
+            'ends with .' => ['person.'],
+            'ends with . on subpath' => ['person.firstName.'],
+            'starting with .' => ['.person.firstName'],
+            'has ..' => ['person..firstName'],
+            'has .. and .' => ['person..firstName.'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidPathProvider
+     */
+    public function it_throws_exception_if_invalid_path_is_used_when_getting_data(string $path): void
+    {
+        $this->expectException(InvalidDataBagPathException::class);
+        $this->dataBag->get($path);
+    }
+
+    public function getProvider(): array
+    {
+        return [
+            ['not.existing', 'default'],
+            ['person.firstName', 'John'],
+            ['person.emailAddresses.0', ['emailAddress' => 'john@doe.com', 'type' => 'private']],
+            ['person.emailAddresses.0.emailAddress', 'john@doe.com'],
+            ['person.emailAddresses.privateGsm.emailAddress', 'john@doe2.com'],
+            ['person.addresses.test', 'default'],
+            ['person.emailAddresses.test', 'default'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider getProvider
+     * @param array|string $expected
+     */
+    public function it_can_get_data_using_a_path(string $path, $expected): void
+    {
+        self::assertEquals($expected, $this->dataBag->get($path, 'default'));
+    }
+
     protected function setUp(): void
     {
         $personData = [
@@ -38,49 +84,5 @@ final class GetFromDataBagTest extends TestCase
     protected function tearDown(): void
     {
         unset($this->dataBag);
-    }
-
-    public function invalidPathProvider(): array
-    {
-        return [
-            'empty' => [''],
-            'non-existant' => ['invalidPath'],
-            'ends with .' => ['person.'],
-            'ends with . on subpath' => ['person.firstName.'],
-            'starting with .' => ['.person.firstName'],
-            'has ..' => ['person..firstName'],
-            'has .. and .' => ['person..firstName.'],
-        ];
-    }
-
-    /**
-     * @dataProvider invalidPathProvider
-     */
-    public function testInvalidPath(string $path): void
-    {
-        $this->expectException(InvalidDataBagPathException::class);
-        $this->dataBag->get($path);
-    }
-
-    public function provider(): array
-    {
-        return [
-            ['not.existing', 'default'],
-            ['person.firstName', 'John'],
-            ['person.emailAddresses.0', ['emailAddress' => 'john@doe.com', 'type' => 'private']],
-            ['person.emailAddresses.0.emailAddress', 'john@doe.com'],
-            ['person.emailAddresses.privateGsm.emailAddress', 'john@doe2.com'],
-            ['person.addresses.test', 'default'],
-            ['person.emailAddresses.test', 'default'],
-        ];
-    }
-
-    /**
-     * @dataProvider provider
-     * @param array|string $expected
-     */
-    public function testDataBagGet(string $path, $expected): void
-    {
-        self::assertEquals($expected, $this->dataBag->get($path, 'default'));
     }
 }
